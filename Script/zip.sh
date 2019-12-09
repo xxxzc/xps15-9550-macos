c5dfe74d42a5c72a3c372733aefa13dbebf31503
zicong xie <xiuscong@gmail.com>
2019-12-10 06:49:21 +0800

    Fix zip script

--- Script/zip.sh
+++ Script/zip.sh
@@ -2,5 +2,5 @@ name=XPS15-9550
 root=$(dirname $0)/.. 
 # python $root/update.py --set $(sh $root/Script/gen_sn_mlb_uuid.sh)
 # random one
-python $root/update.py $name --set sn=C02VK0ZLGTFN mlb=C02742306QXHCF9JC smuuid=C167D3A2-CC13-4041-8CED-553D772C0749
+python $root/update.py $1 --set sn=C02VK0ZLGTFN mlb=C02742306QXHCF9JC smuuid=C167D3A2-CC13-4041-8CED-553D772C0749
 cd $root && zip -r ${name}-$1-$(date +%y%m).zip $1 Tool Script README.md Changelog.md update.py packages.csv
